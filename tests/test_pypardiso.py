05473f4a79b78a8c7bc0d7ed87c9c1f7d8798ad8
Adrian Haas <haasad@student.ethz.ch>
2016-09-29 16:18:51 +0200

    combined all tests in one file, in order to only have on instance of PyPardisoSolver, which i suspect is the reason for the segfaults on travis

--- tests/test_pypardiso.py
+++ tests/test_pypardiso.py
@@ -4,10 +4,13 @@ import pytest
 import numpy as np
 import scipy.sparse as sp
 from scipy.sparse import SparseEfficiencyWarning
+from scipy.sparse.linalg import spsolve as scipyspsolve
+from scipy.sparse.linalg import factorized as scipyfactorized
 
 from pypardiso.pardiso_wrapper import PyPardisoSolver, PyPardisoError, PyPardisoWarning
+from pypardiso.scipy_aliases import pypardiso_solver, spsolve, factorized
 
-ps = PyPardisoSolver()
+ps = pypardiso_solver
 
 
 def create_test_A_b(matrix=False, sort_indices=True):
@@ -227,3 +230,49 @@ def test_no_stale_factorization_used_hashing():
     assert ps.phase == 13
     assert not np.allclose(x1, x2)
     ps.size_limit_storage = 5e7
+
+
+# scipy aliases
+
+def test_basic_spsolve_vector():
+    pypardiso_solver.remove_stored_factorization()
+    A, b = create_test_A_b()
+    xpp = spsolve(A,b)
+    xscipy = scipyspsolve(A,b)
+    np.testing.assert_array_almost_equal(xpp, xscipy)
+
+
+def test_basic_spsolve_matrix():
+    pypardiso_solver.remove_stored_factorization()
+    A, b = create_test_A_b(matrix=True)
+    xpp = spsolve(A,b)
+    xscipy = scipyspsolve(A,b)
+    np.testing.assert_array_almost_equal(xpp, xscipy)
+
+
+def test_basic_factorized():
+    pypardiso_solver.remove_stored_factorization()
+    A, b = create_test_A_b()
+    ppfact = factorized(A)
+    xpp = ppfact(b)
+    scipyfact = scipyfactorized(A)
+    xscipy = scipyfact(b)
+    np.testing.assert_array_almost_equal(xpp, xscipy)
+
+
+def test_factorized_modified_A():
+    pypardiso_solver.remove_stored_factorization()
+    assert pypardiso_solver.factorized_A.shape == (0,0)
+    A, b = create_test_A_b()
+    Afact = factorized(A)
+    assert pypardiso_solver.factorized_A.shape == A.shape
+    x1 = Afact(b)
+    A[4,0] = 27
+    x2 = spsolve(A, b)
+    assert not np.allclose(x1, x2)
+    assert pypardiso_solver.factorized_A[4,0] == 27
+    x3 = Afact(b)
+    np.testing.assert_array_equal(x1, x3)
+    assert pypardiso_solver.phase == 13 # this is not the desired situation, because now every call to Afact
+                                        # will be carried out with phase 13, ie pardiso will factorize everytime
+                                        # -> needs to be documented in list of caveats or similar
