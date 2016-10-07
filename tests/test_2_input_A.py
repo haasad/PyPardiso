d8c45755fe92b6123841f5ec5d9f499348facf22
Adrian Haas <haasad@student.ethz.ch>
2016-10-07 14:12:52 +0200

    some more overworked tests

--- tests/test_2_input_A.py
+++ tests/test_2_input_A.py
@@ -4,6 +4,7 @@ import pytest
 import numpy as np
 import scipy.sparse as sp
 from utils import create_test_A_b_small, create_test_A_b_rand, basic_solve
+from pypardiso.pardiso_wrapper import PyPardisoWarning, PyPardisoError
 from pypardiso.scipy_aliases import pypardiso_solver
 
 ps = pypardiso_solver
@@ -24,12 +25,13 @@ def test_input_A_non_sparse():
     with pytest.raises(TypeError):
         ps.solve(A,b)
 
+
 def test_input_A_csc():
-	A, b = create_test_A_b_rand()
-	x_csr = ps.solve(A,b)
-	Acsc = A.copy().asformat('csc')
-	x_csc = ps.solve(Acsc, b)
-	np.testing.assert_array_almost_equal(x_csr, x_csc)
+    A, b = create_test_A_b_rand()
+    x_csr = ps.solve(A,b)
+    Acsc = A.copy().asformat('csc')
+    x_csc = ps.solve(Acsc, b)
+    np.testing.assert_array_almost_equal(x_csr, x_csc)
 
 
 def test_input_A_other_sparse():
@@ -50,10 +52,36 @@ def test_input_A_empty_row_and_col():
         ps.solve(Asp,b)
 
 
+def test_input_A_empty_row():
+    A, b = create_test_A_b_rand(25, 0.7)
+    A = np.array(A.todense())
+    A[0,:] = 0
+    A[1, 0] = 1
+    Asp = sp.csr_matrix(A)
+    with pytest.raises(ValueError):
+        basic_solve(Asp,b)
 
 
+def test_input_A_dtypes():
+    A, b = create_test_A_b_rand(10,0.5)
+    for d in [np.float16, np.float32, np.int16, np.int32, np.int64, np.complex64, 
+              np.complex128, np.complex128, np.uint16, np.uint32, np.uint64]:
+        with pytest.raises(TypeError):
+            ps.solve(A.astype(d), b)
 
 
+#def test_input_A_empty_col():
+#    A, b = create_test_A_b_rand(25, 0.1)
+#    A = np.array(A.todense())
+#    A[0,:] = 0
+#    A[:,0] = 0
+#    A[0, 1] = 1
+#    Asp = sp.csr_matrix(A)
+#    with pytest.warns(PyPardisoWarning):
+#        x = ps.solve(A,b)
+#        print(x)
+
+# dosen't necessarily return inf value, can also be just really big
 
 
 def test_input_A_nonsquare():
---
+++ tests/test_3_input_b.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+
+import pytest
+import numpy as np
+import scipy.sparse as sp
+from scipy.sparse import SparseEfficiencyWarning
+from utils import create_test_A_b_small, create_test_A_b_rand, basic_solve
+from pypardiso.pardiso_wrapper import PyPardisoWarning, PyPardisoError
+from pypardiso.scipy_aliases import pypardiso_solver
+
+ps = pypardiso_solver
+
+
+def test_input_b_sparse():
+    A, b = create_test_A_b_rand()
+    bsparse = sp.csr_matrix(b)
+    with pytest.warns(SparseEfficiencyWarning):
+        x = ps.solve(A, bsparse)
+        np.testing.assert_array_almost_equal(A*x, b)
+
+
+def test_input_b_shape():
+    A, b = create_test_A_b_rand()
+    x_array = ps.solve(A,b)
+    assert x_array.shape == b.shape
+    x_vector = ps.solve(A, b.squeeze())
+    assert x_vector.shape == b.squeeze().shape
+    np.testing.assert_array_equal(x_array.squeeze(), x_vector)
+
+
+def test_input_b_dtypes():
+    A, b = create_test_A_b_rand()
+    for dt in [np.float16, np.float32, np.int16, np.int32, np.int64]:
+        bdt = b.astype(dt)
+        with pytest.warns(PyPardisoWarning):
+            basic_solve(A, bdt)
+            
+    for dt in [np.complex64, np.complex128, np.complex128, np.uint16, np.uint32, np.uint64]:
+        bdt = b.astype(dt)
+        with pytest.raises(TypeError):
+            basic_solve(A, bdt)
+
+
+def test_input_b_fortran_order():
+    A, b = create_test_A_b_rand(matrix=True)
+    x = ps.solve(A,b)
+    xfort = ps.solve(A, np.asfortranarray(b))
+    np.testing.assert_array_equal(x, xfort)
+
+
+def test_input_b_wrong_shape():
+    A, b = create_test_A_b_rand()
+    b = np.append(b, 1)
+    with pytest.raises(ValueError):
+        basic_solve(A,b)
+    
+
