21d18fca505b2594be38d39a2af7b240910f3cc5
Adrian Haas <haasad@student.ethz.ch>
2016-10-07 17:33:47 +0200

    removed the free-memory again from test_1_basic_solve.

--- tests/test_1_basic_solve.py
+++ tests/test_1_basic_solve.py
@@ -9,10 +9,8 @@ ps = pypardiso_solver
 def test_bvector_smoketest():
 	A, b = create_test_A_b_rand()
 	basic_solve(A,b)
-	ps.free_memory()
 	
 
 def test_bmatrix_smoketest():
     A, b = create_test_A_b_rand(matrix=True)
     basic_solve(A,b)
-    ps.free_memory()
