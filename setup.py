ee297a7e3eb857006361120c153ec88a3ecbee4b
haasad <haasad@users.noreply.github.com>
2021-10-24 13:06:07 +0200

    Bump version number to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pypardiso',
-    version="0.3.3",
+    version="0.4.0",
     packages=['pypardiso'],
     install_requires=['mkl', 'numpy', 'scipy'],
     author="Adrian Haas",
