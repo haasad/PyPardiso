88f4476894239fa9017c2a842e6b6637650b3308
haasad <haasad@users.noreply.github.com>
2023-11-16 21:31:05 +0100

    Use importlib to determine package version

--- pypardiso/__init__.py
+++ pypardiso/__init__.py
@@ -1,8 +1,14 @@
 # coding: utf-8
+from importlib.metadata import version, PackageNotFoundError
 
 from .pardiso_wrapper import PyPardisoSolver
 from .scipy_aliases import spsolve, factorized
 from .scipy_aliases import pypardiso_solver as ps
 
-__version__ = '0.4.3'
+
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+
 __all__ = ['PyPardisoSolver', 'spsolve', 'factorized', 'ps']
